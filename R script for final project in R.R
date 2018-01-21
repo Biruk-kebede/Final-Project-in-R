7cc0deeed6599051caf3f86ae407d6b27ba72314
Biruk-Kebede <birkebe2004@gmail.com>
2018-01-21 19:36:42 +0100

    second edition

--- R script for final project in R.R
+++ R script for final project in R.R
@@ -3,16 +3,16 @@ library(broom)
 library(psych)
 library(stargazer)
 library(car)
-install.packages("pander")
-require(knitr)
+library(knitr)
+library(ggfortify)
 
 #working directory adjusted
-setwd("/Users/biruk/Dropbox/Final_R_Project")
+setwd("/Users/biruk/Dropbox/Final Project in R")
 
 #importing the data
-given_dataset<- read_csv(file="/Users/biruk/Dropbox/Final_R_Project/xAPI-Edu-Data.csv")
+given_dataset<- read_csv(file="/Users/biruk/Dropbox/Final Project in R/xAPI-Edu-Data.csv")
 
-library(stargazer)
+
 #Exploratory Data Analysis -----------------------------------------------
 
 # study the data using summary function 
@@ -75,7 +75,7 @@ boxplot.stats(stud_performance_data$Discussion)
 pie(table(stud_performance_data$Topic))
 
 ## Participants citizenship composition 
-barplot(table(stud_performance_data$NationalITy), xlab = 'citizenship', ylab = 'Number Of Participants')
+barplot(table(stud_performance_data$NationalITy), xlab = 'participants citizenship', ylab = 'Number Of Participants')
 
 ## check for normality of quantitative variables in the dataset
 plot (density(stud_performance_data$raisedhands))
@@ -140,23 +140,24 @@ plot(subject, final_grade, ylab = "final_grade", xlab = "subject")
 # Independent observations is automatic as the two observations are completely independent (male and female)
 #Based on the central limit theorem if the sample size is large enough (n > 30), we can ignore the distribution of the data and use parametric tests.
   ## Do box plot to examine the equivalence of variance between the two groups
-boxplot(raisedhands~sex) 
+boxplot(stud_performance_data$raisedhands~stud_performance_data$sex) 
 
 # the boxplot indicates that the variances of the two groups are almost the same. This can also be verified usng Levene's Test
-leveneTest(raisedhands~sex) 
+leveneTest(stud_performance_data$raisedhands~sex) 
 
 # it is confirmed that the difference in variance is not significant  
 
 ## Now conduct t-test to verify/refute the hypothesis
-  t.test(raisedhands~sex, mu=0, alt="two.sided", conf=0.95, var.eq=T, paired=F)
+  t.test(stud_performance_data$raisedhands~sex, mu=0, alt="two.sided", conf=0.95, var.eq=T, paired=F)
 ## to include SD statistics in the report calculate it 
-sd(raisedhands[sex=="F"])
-sd(raisedhands[sex=="M"])
+sd(stud_performance_data$raisedhands[sex=="F"])
+sd(stud_performance_data$raisedhands[sex=="M"])
    
   
 ##correlation--------------------------------------------------------
 
-# Is there a statistically significant relationship between students announcement view and participation in discussion?
+# Is there a statistically significant relationship between announcement view and participation in discussion?
+
 ## check for the density distribution of the variables to check for normality to select appropriate technique of correlation
 plot((density(stud_performance_data$AnnouncementsView)), main="Kernel Density of announcement view distribution")
 plot((density(stud_performance_data$Discussion)), main="Kernel Density of Discussion distribution")
@@ -173,53 +174,105 @@ qqline(log_AnnouncementsView, col ="red")
 ## still the transformation of the scores in both variable indicated that they are normally distributed after transformation
 
 ## plot to examine monotonic relationship between the two variables to use Kendall's tau. 
-plot(Discussion, AnnouncementsView, main="scatterplot", las=1)
+plot(stud_performance_data$Discussion, stud_performance_data$AnnouncementsView, main="scatterplot", las=1)
 # the plot shows that it fulfilled the assumption of monotonic relationship 
 
 # conduct correlation test
-cor.test(Discussion, AnnouncementsView, method="kendall")
+cor.test(stud_performance_data$Discussion,  stud_performance_data$AnnouncementsView, method="kendall")
 
 ## correlation test depicted that there is signficant correlation between the two variables (tau=0.2851579, p<.0001)
+
+## plot the result by using different color for gender just for the sake of informativity of the plot
+
+stud_performance_data %>% 
+  ggplot() +
+  aes(x = AnnouncementsView, y = Discussion) +
+  geom_point(aes(color = gender), size = 3) +
+  geom_smooth(method = "lm")
+
 
 
 # Regression Analysis Part ------------------------------------------------
   
 # Do resource visiting and announcement viewing significantly predict students hand raising behavior? 
 
+## Before conducting linear regression check the assumptions of it
 #the relationship among the variables used in the hypothesis was studied using plot matrix (used to check the linearity of relationship between IVs with DV)
   plot(stud_performance_data[5:7], pch=16, col="blue", main="Matrix Scatterplot of raisedhands, VisITedResources, AnnouncementsView")
 
 ##As can be seen from the plot matrix the assumption of linear relationship between IVS and DV is fulfilled.
 
-# Explore the jointed effect of resources visiting and announcement vewing on handrising using plot
-coplot(raisedhands~VisITedResources|AnnouncementsView, panel = panel.smooth, stud_performance_data)
-coplot(raisedhands ~AnnouncementsView|VisITedResources, panel = panel.smooth, stud_performance_data)
-
-### Check for multicollinearity of independent variables using correlation matrix 
+## The next step could be to check for multicollinearity of independent variables. 
+## It would be better to start this process by looking at correlation matrix among the variables 
 library(corrplot)
 check_cor = cor(stud_performance_data[5:7])
 corrplot(check_cor, method = "number")
-## the correlations between the independent variables are below the cutoff (0.8)
+## As can be seen from the correlation matrix the correlation between the two independent variables is 0.59, which is below the cutoff (.8) 
 
-#Construct a Linear model and conduct residual diagnosis 
-# fit the first model and conduct the corresponding residual diagnosis
+
+#Fit a Linear model and continue testing its assumptions  
 lm1<-lm(raisedhands~VisITedResources + AnnouncementsView, data=stud_performance_data)
-par(mfrow=c(2, 2))
-plot(lm1)
 summary(lm1)
 
-# the p value indicates that both independent variables contribute significantly to the model and hence both of them should be keept.
-# both predictors and their interaction together are fited to the second model
-lm2<-lm(raisedhands~VisITedResources*AnnouncementsView, data=stud_performance_data)
-par(mfrow=c(2, 2))
-plot(lm2)
-summary(lm2)
+# now assess multicollinearity using the variance inflaction factor (VIF)
+car::vif(lm1)
+# The values look ok as they are not very large. 
 
-# only interaction between the two predictors 
+# However as the average of VIF is larger than 1, it seems that multicollineratity is a bit biasing the model
+mean(car::vif(lm1))
+
+#So, look at the tolerance.
+1/car::vif(lm1)
+# The tolerance value seems it is good as it is in the range of 0 and 1.
+
+# now, assess the independence of residuals
+car::dwt(lm1)
+# The result indicates that there seems autocorrelation between the the two independent variables, so the residuals may not be independent.
+
+# To check heteroscetasticity inspect the residual diagnostic plots
+autoplot(lm1, which = 1:6, label.size = 3)
+
+#Examine the outliers if there is concrete reason to eliminate theme. There is no concrete reason to eliminate them. 
+stud_performance_data %>% 
+  slice(c(96, 178, 187, 345, 382))
+
+
+#Now, fit the second model which consists of both predictors and their interaction together
+lm2<-lm(raisedhands~VisITedResources*AnnouncementsView, data=stud_performance_data)
+summary(lm2)
+# the p value indicates that the interaction between the two independent variables didn't significantly contribute to the model.
+
+
+# The VIF of the model indicated that the interaction between the two variables has a score of 15.007, which is larger than 10. So, multicollinearity is an issue here.  
+car::vif(lm2)
+
+# In addition, the the average of VIF score (9.273) confirms that multicollineratity is biasing the model.
+mean(car::vif(lm2))
+
+## The tolerance also indicates that except visiting resources the other two variables (announcement and interaction between announcement view and visiting resource) have not tolerable.1/car::vif(lm2)
+
+# Measuring the independence of residuals
+car::dwt(lm2)
+# It seems that the model has some significant autocorrelation, so the residuals are not independent
+
+# To check heteroscetasticity inspect the residual diagnostic plots
+
+autoplot(lm2, which = 1:6, label.size = 3)
+
+# Fit the third model which includes only the interaction between the two predictors and continue testing linear regression model assumptions 
 lm3<-lm(raisedhands~VisITedResources:AnnouncementsView, data=stud_performance_data)
-par(mfrow=c(2, 2))
-plot(lm3)
 summary(lm3)
+
+# In this model the p value indicates that the interaticon of the independent variables significantly contribute to the model.
+
+# Assess the independence of residuals in the model
+car::dwt(lm3)
+# The result indicates that there seems autocorrelation between the the two independent variables, so the residuals may not be independent again.
+
+# To check heteroscetasticity inspect the residual diagnostic plots
+
+autoplot(lm3, which = 1:6, label.size = 3)
+
 
 # confidence intervals for parameters
 confint(lm1, level = 0.95)
@@ -257,9 +310,44 @@ stud_performance_data %>%
   shapiro.test(.)
 # The Shapiro-Wilks test confirmed that the residuals are normally distributed (p=0.07819)
 
-# report the results of regression in table
-library(stargazer) 
 
+# Explore the jointed effect of resources visiting and announcement vewing on handrising using plot
+coplot(raisedhands~VisITedResources|AnnouncementsView, panel = panel.smooth, stud_performance_data)
+coplot(raisedhands ~AnnouncementsView|VisITedResources, panel = panel.smooth, stud_performance_data)
+
+
+
+
+
+
+
+
+
+
+##By loading important packages plot the result of the model in 3D.
+
+library(plotly)
+plot_ly(stud_performance_data, y= ~stud_performance_data$raisedhands, x= ~stud_performance_data$AnnouncementsView, z= ~stud_performance_data$VisITedResources)
+
+install.packages("scatterplot3d")
+library(scatterplot3d)
+install.packages("rgl")
+library(rgl)
+library(car)
+
+scatter3d(y=stud_performance_data$raisedhands, x=stud_performance_data$AnnouncementsView, z=stud_performance_data$VisITedResources)
+
+
+
+
+
+
+
+
+
+
+
+# report the results of regression in table
 Models_table_html <-
   stargazer(lm1,
             lm2,
@@ -276,7 +364,10 @@ Models_table_html <-
             digits = 2,
             type = "html")
 
-write_lines(trial_table_html, "trial_table.html")
+write_lines(Models_table_html, "Models_table_html")
+
+
+stargazer(lm1, lm2, lm3, title = "Model comparison", align = TRUE, type = "text")
 
 # also add standardized coefficients
 # transform the non-standardized values using the lm.beta package
